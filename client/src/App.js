4994f55462a39b31b720e5e0dad94b75348196fa
manuraj17 <manu.elayath@gmail.com>
2017-10-02 16:28:14 +0530

    add client component to display tweets

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,67 @@
 import React, { Component } from 'react';
+import { Image, List, Container, Header } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
+import socketIOClient from "socket.io-client";
 import './App.css';
 
+const io = require('socket.io-client'); //('http://localhost:3000/');
+
 class App extends Component {
-  constructor(props){
-    super(props);
-    console.log("Constructor");
-    this.state = { users: [] };
+  constructor(props) {
+   super(props);
+
+   this.state = {
+     tweets: [],
+   }
+   this.socket = io.connect();
+   this.socket.on('news', data => {
+     this.updateTweet(data);
+   });
+  }
+ 
+  updateTweet(data) {
+   let tweets = this.state.tweets;
+   if (tweets.length > 25) { 
+     console.log("Size exceeded, popping");
+     tweets.shift(); 
+   }
+   tweets.push(data);
+   this.setState({tweets: tweets});
   }
 
-  componentDidMount(){
-    fetch('/users')
-    .then(res => res.json())
-    .then(users => {
-      console.log("Recieved: ", users);
-      this.setState({users})
-    });
+  onSocketOpen() {}
+
+  onSocketData(e) {
+   console.log(JSON.parse(e));
+   this.setState({tweets: JSON.parse(e)});
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user => 
-          <div key={user.id}>{user.username}</div>
-        )}
+  componentWillUnmount() {
+    this.ws.close();
+  }
+
+  render(){
+    const numbers = this.state.tweets;
+    let list = [];
+    list = numbers.map(e => 
+      <List.Item key={e.id.toString()}>
+        <Image avatar src={e.image} />
+        <List.Content>
+          <List.Header>{e.user}</List.Header>
+          <List.Description>{e.text}</List.Description>
+        </List.Content>
+      </List.Item>
+    ).reverse();
+    return(
+      <div>
+        <Container>
+        <Header as='h1'>#javascript</Header>
+        <List>
+          {list}
+        </List>
+        </Container>
       </div>
-    );
+    )
   }
 }
 
